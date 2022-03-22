71e9a10debc42ae1905f7c03f19cadd8b3b6e5a2
Tom Sutton <tomsutton1984@gmail.com>
2022-03-23 11:20:41 +1300

    List commands used to create repo

---
+++ command.sh
@@ -0,0 +1,10 @@
+npx create-react-app forage_task_1
+cd forage_task_1
+code .
+gh repo create forage_task_1 --private --push --source .
+git branch update_logo
+git switch update_logo
+git commit -a -m "Vanilla create-react-up with updated logo and link"
+gh pr create -B master
+gh pr merge
+git commit -a -m "List commands used to create repo"
